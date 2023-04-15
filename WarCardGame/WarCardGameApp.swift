cfede5ed75af4ec8c84137a82bca8953b2e90096
salmadwv101 <sfalorifi22@twq.idserve.net>
2023-04-15 20:58:16 +0300

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by salma alorifi on 24/09/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
